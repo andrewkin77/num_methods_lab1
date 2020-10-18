a5e36efeadb1ef8d4212aa142990ea0e50c36e7b
Andrew Lebedev <jediandrewkin@gmail.com>
2020-10-18 18:52:03 +0300

    Reworked UI, table works

--- lab1_GUI.py
+++ lab1_GUI.py
@@ -3,55 +3,104 @@ import PySimpleGUI as sg
 
 sg.theme('DarkBlue14')   # Add a touch of color
 # All the stuff inside your window.
-def make_table(is_test_table):
+def make_table(is_test_table, data):
+	col_widths = [5,5,5,5,5,5,5,5]
+	headers = [' № ', 'x', 'v', 'v2', 'v-v2','LE', ' h ', ' C1 ', ' C2 ', 'u', '|u-v|']
+	print(len(data[0]))
+	print(len(data[6]))
+	print(len(data))
 	if is_test_table:
-			headers = ['x', 'v']
-			table_data = [[data[j][i] for j in range(2)] for i in range(len(data[0]))]
-			layout = [
-						[
-						 sg.Table(values=table_data, headings=headers, display_row_numbers=True, num_rows=15)
-						],
-						[
-						 sg.Button('Exit', size = (6,1), font=('Avenir 15'))
-						]
-					]
+		col_map = [1 for i in range(len(headers))]
+	else:
+		col_map = [1 for i in range(len(headers)-2)]
+	table_data = [ [0 for col in range(len(headers))] for row in range(len(data[0]))]
+	for row in range(len(data[0])):
+			table_data[row][0] = '{:d}'.format(row+1) # step number
+			table_data[row][1] = '{:.5f}'.format(data[0][row]) # x
+			table_data[row][2] = '{:.5f}'.format(data[1][row]) # v
+			table_data[row][3] = '{:.5f}'.format(data[2][row]) # v2
+			table_data[row][4] = '{:.2E}'.format(data[1][row] - data[2][row]) # v-v2
+			table_data[row][5] = '{:.2E}'.format(data[3][row]) #LE
+			table_data[row][6] = '{:.2E}'.format(data[4][row]) # h
+			table_data[row][7] = '{:d}'.format(data[5][row]) # C1
+			table_data[row][8] = '{:d}'.format(data[6][row]) # C2
+			if is_test_table:
+				table_data[row][9] = '{:.5f}'.format(data[len(data)-1][row]) # u if is_test
+				table_data[row][10] = '{:.2E}'.format(abs(data[len(data)-1][row] - data[1][row])) # |u - v| if is_test
+
+	layout = [
+				[
+				 sg.Table(values=table_data, headings=headers, auto_size_columns=True,
+				 	col_widths=None, num_rows=15, vertical_scroll_only=True,
+				 	visible_column_map=col_map)
+				],
+				[
+				 sg.Button('Close', size = (6,1), font=('Avenir 15'))
+				]
+			]
 	return layout				
 
+top_frame_layout = [ 
+	[ 
+	 sg.Text('x0:'), sg.InputText('0', size = (7,1), key = 'input_x0'),
+     sg.Text('Right boundary:'), sg.InputText('5', size = (7,1),
+     	key = 'input_rb'),
+    ],
+
+    [
+     sg.Text('u0:'), sg.InputText('3', size = (7,1), key = 'input_y0'),
+     sg.Text('Step:'), sg.InputText('0.1', size = (7,1), key = 'input_h')
+    ],
+
+    [
+     sg.Text('Function:'),
+     sg.Radio('Test', "RADIO1", default=True, key='RTest'),
+ 	 sg.Radio('Function 1', "RADIO1", key='RFunc1'),
+ 	 sg.Radio('Function 2', "RADIO1", key='RFunc2')
+ 	]
+]
+
+bottom_frame_layout = [ 
+	[
+	 sg.Checkbox('Use error control', default=False,
+	 	enable_events = True, key='cbox_bool')
+	],
+
+	[
+	 sg.Text('Max error:'), sg.InputText('', size = (7,1), disabled = True,
+		 disabled_readonly_background_color= sg.theme_background_color(), key = 'input_e'),
+	 sg.Text('Number of iterations:'), sg.InputText('', size = (7,1), disabled = True,
+		 disabled_readonly_background_color= sg.theme_background_color(), key = 'input_N')
+	] 
+]
+
+input_frame_layout = [ 
+	[
+     sg.Frame(layout=top_frame_layout, title = '', border_width=0)
+    ],
+
+    [
+     sg.Frame(layout=bottom_frame_layout, title='Error comtrol')
+    ]
+]
+
 layout_main = [  
+			[
+				sg.Frame(layout=input_frame_layout, title='', border_width=0)
+			],
+
             [
-             sg.Text('x0:'), sg.InputText('0', size = (7,1), key = 'input_x0'),
-             sg.Text('Right boundary:'), sg.InputText('', size = (7,1),
-             	key = 'input_rb'),
+             sg.Text('', size=(1,1), font = ('Avenir 10'))
             ],
 
             [
-             sg.Text('u0:'), sg.InputText('', size = (7,1), key = 'input_y0'),
-             sg.Text('Step:'), sg.InputText('', size = (7,1), key = 'input_h')
-            ],
-
-            [
-             sg.Text('Function:'),
-             sg.Radio('Test', "RADIO1", default=True, key='RTest'),
-         	 sg.Radio('Function 1', "RADIO1", key='RFunc1'),
-         	 sg.Radio('Function 2', "RADIO1", key='RFunc2')
-            ],
-
-            [sg.Checkbox('Use error control', default=False,
-            	enable_events = True, key='cbox_bool')],
-
-            [
-             sg.Text('Max error:'), sg.InputText('', size = (7,1), disabled = True,
-             	disabled_readonly_background_color= sg.theme_background_color(), key = 'input_e'),
-             sg.Text('Number of iterations:'), sg.InputText('', size = (7,1), disabled = True,
-             	disabled_readonly_background_color= sg.theme_background_color(), key = 'input_N')
-            ],
-
-            [
-             sg.Button('Exit', size = (6,1), font=('Avenir 15')), 
+             sg.Button('Exit', size = (6,1), font=('Avenir 15')),
+             sg.Text('', size  = (17,1), font = ('Avenir 15')),
              sg.Button('Table', size = (6,1), font=('Avenir 15'), disabled = True),
              # sg.Button('Clear', size = (6,1), font=('Avenir 15'), disabled = True),
+             sg.Button('Plot', size = (4,1), font=('Avenir 15'), disabled = True),
              sg.Button('Calculate', size = (7,1), font=('Avenir 15'), bind_return_key = True)
-            ] 
+            ]
         ]
 
 # Create main window
@@ -74,7 +123,7 @@ while True:
 	        window_main.FindElement('input_N').Update(disabled = True)
 
 	if event == 'Table':
-		window_table = sg.Window('Table', make_table(is_test_table), font=('Avenir 17'))
+		window_table = sg.Window('Table', make_table(is_test_table, data), font=('Avenir 17'))
 		while True:
 			event_t, values_t = window_table.read()
 			if event_t == sg.WIN_CLOSED or event_t == 'Close':
@@ -82,7 +131,9 @@ while True:
 		window_table.close()
 
 	if event == 'Calculate':
+		is_test_table = False
 		window_main.FindElement('Table').Update(disabled = False)
+		window_main.FindElement('Plot').Update(disabled = False)
 		# window_main.FindElement('Clear').Update(disabled = False)
 
 		#user input
@@ -105,6 +156,8 @@ while True:
 		elif values['RFunc1']:
 			data = ln.func_num_sln(x0, u0, x_max, h, Nmax, e,
 				ln.func_1, values['cbox_bool'])
+
+	if event == 'Plot':
 		ln.draw(data, values['cbox_bool'], values['RTest'])
 		
 
--- lab1_num.py
+++ lab1_num.py
@@ -10,20 +10,29 @@ def func_1(x, y):
     return a * (y**2) + y - (y**3) * math.sin(10*x)
 
 def RK4(x_i, y_i, h, func):
-    y = y_i
-    k1 = h * func(x_i, y)
-    k2 = h * func(x_i + h/2, y + k1/2)
-    k3 = h * func(x_i + h/2, y + k2/2)
-    k4 = h * func(x_i + h, y + k3)
-	if (abs(k1) + abs(k2) + abs(k3) + abs(k4) > 10000):
-		return 10000
-    y += (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-    return y
+	v_max = 10000
+	y = y_i
+	k1 = h * func(x_i, y)
+	k2 = h * func(x_i + h/2, y + k1/2)
+	k3 = h * func(x_i + h/2, y + k2/2)
+	k4 = h * func(x_i + h, y + k3)
+	if (abs(k1) + abs(k2) + abs(k3) + abs(k4) > v_max):
+		return v_max
+	y += (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+	return y
 
 def func_num_sln(x0, u0, x_max, h, Nmax, max_error, func, error_control):
+	v_max = 10000
+	c1 = 0
+	c2 = 0
+	x = x0
+	v = u0
+	v2 = u0
+	i = 1
+
 	X = []
 	V = []
-	X2 = []
+	#X2 = []
 	V2 = []
 	C1 = []
 	C2 = []
@@ -32,23 +41,19 @@ def func_num_sln(x0, u0, x_max, h, Nmax, max_error, func, error_control):
 
 	X.append(x0)
 	V.append(u0)
-	X2.append(x0)
+	#X2.append(x0)
 	V2.append(u0)
+	H.append(h)
+	C1.append(c1)
+	C2.append(c2)
+	Error_arr.append(abs(v2 - v) / 15)
 
-	#v_max = 10e3
-	c1 = 0
-	c2 = 0
-	x = x0
-	v = u0
-	v2 = u0
-	i = 1
-
-	while x < x_max:
+	while x <= x_max - h:
 		temp = RK4(x, v, h, func)
 		temp2 = RK4(x, v2, h/2, func)
-		v_half = temp2
+		#v_half = temp2
 		temp2 = RK4(x + h/2, temp2, h/2, func)
-		if temp == 10000 or temp2 == 10000:
+		if temp == v_max or temp2 == v_max:
 			break
 		if error_control and abs(temp2 - temp) > max_error:
 			h /= 2
@@ -59,9 +64,9 @@ def func_num_sln(x0, u0, x_max, h, Nmax, max_error, func, error_control):
 		v2 = temp2
 		X.append(x)
 		V.append(v)
-		X2.append(x-h/2)
-		V2.append(v_half)
-		X2.append(x)
+		#X2.append(x-h/2)
+		#V2.append(v_half)
+		#X2.append(x)
 		V2.append(v2)
 		C1.append(c1)
 		H.append(h)
@@ -70,14 +75,15 @@ def func_num_sln(x0, u0, x_max, h, Nmax, max_error, func, error_control):
 			if abs(v2 - v) < (max_error/32):
 				h *= 2
 				c2 += 1
+		C2.append(c2)
+		if error_control:
 			if i == Nmax:
 				break
 		i += 1
-		C2.append(c2)
 		#if abs(v) > v_max:
 			#break
 
-	data = [X, V, X2, V2, Error_arr, C1, C2]
+	data = [X, V, V2, Error_arr, H, C1, C2]
 	return data
 
 def test_precise_sln(x0, u0, h, x_max):
@@ -100,7 +106,7 @@ def test_precise_sln(x0, u0, h, x_max):
 def draw(data, error_control, is_test):
 	plt.plot(data[0], data[1], label='Regular step')
 	if error_control:
-		plt.plot(data[2], data[3], label='Half Step')
+		plt.plot(data[0], data[2], label='Half Step')
 	if is_test:
 		plt.plot(data[len(data)-2], data[len(data)-1], label='Precise solution')
 	plt.xlabel('x')
