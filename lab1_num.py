6d006cb74df92dfc64513e93927350448e0fe3f8
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2020-10-25 17:30:59 +0300

    error array and max value fixes

--- lab1_num.py
+++ lab1_num.py
@@ -21,38 +21,38 @@ def f_2(x, u_1, u_2, a, b):
 
 
 def RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h, v_max):
-    u1 = np.longdouble(u1_0)
-    u2 = np.longdouble(u2_0)
+    u1 = u1_0
+    u2 = u2_0
     k_1 = np.longdouble(h * f_1(x_0, u1_0, u2_0, a, b))
     if abs(k_1) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     l_1 = np.longdouble(h * f_2(x_0, u1_0, u2_0, a, b))
     if abs(l_1) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     k_2 = np.longdouble(h * f_1(x_0 + h / 2, u1_0 + k_1 / 2, u2_0 + l_1 / 2, a, b))
     if abs(k_2) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     l_2 = np.longdouble(h * f_2(x_0 + h / 2, u1_0 + k_1 / 2, u2_0 + l_1 / 2, a, b))
     if abs(l_2) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     k_3 = np.longdouble(h * f_1(x_0 + h / 2, u1_0 + k_2 / 2, u2_0 + l_2 / 2, a, b))
     if abs(k_3) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     l_3 = np.longdouble(h * f_2(x_0 + h / 2, u1_0 + k_2 / 2, u2_0 + l_2 / 2, a, b))
     if abs(l_3) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     k_4 = np.longdouble(h * f_1(x_0 + h, u1_0 + k_3, u2_0 + l_3, a, b))
     if abs(k_4) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     l_4 = np.longdouble(h * f_2(x_0 + h, u1_0 + k_3, u2_0 + l_3, a, b))
     if abs(l_4) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     u1 += np.longdouble(1 / 6 * (k_1 + 2 * k_2 + 2 * k_3 + k_4))
     if abs(u1) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     u2 += np.longdouble(1 / 6 * (l_1 + 2 * l_2 + 2 * l_3 + l_4))
     if abs(u2) > v_max:
-        return u1_0, u2_0
+        return v_max, v_max
     return u1, u2
 
 
@@ -60,9 +60,11 @@ def num_sol_3_task(a, b, N_max, f_1, f_2, x_0, u1_0, u2_0, x_end, h, e, error_co
     v_max = 10e30
     c1 = 0
     c2 = 0
-    u1_ds = 0
-    u2_ds = 0
+    u1_ds = u1_0
+    u2_ds = u2_0
     S_nor = 0
+    counter = 1
+
     C1 = [c1]
     C2 = [c2]
     U1 = [u1_0]
@@ -74,42 +76,44 @@ def num_sol_3_task(a, b, N_max, f_1, f_2, x_0, u1_0, u2_0, x_end, h, e, error_co
     H = [h]
     Error_arr = [0]
     X = [x_0]
-    counter = 0
 
-    while x_0 <= x_end:
-        u1_0, u2_0 = RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h, v_max)
-        if (error_control):
-            u1_ds, u2_ds = RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h / 2, v_max)
-            u1_ds, u2_ds = RK4_s(x_0, u1_ds, u2_ds, f_1, f_2, a, b, h / 2, v_max)
-            S_nor = abs(((u1_ds - u1_0) ** 2 + (u2_ds - u2_0) ** 2) ** (1 / 2))
-            while S_nor > e:
-                h = h / 2
-                u1_0, u2_0 = RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h, v_max)
-                u1_ds, u2_ds = RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h / 2, v_max)
-                u1_ds, u2_ds = RK4_s(x_0, u1_ds, u2_ds, f_1, f_2, a, b, h / 2, v_max)
-                S_nor = abs(((u1_ds - u1_0) ** 2 + (u2_ds - u2_0) ** 2) ** (1 / 2))
-                c1 += 1
-
-        x_0 = x_0 + h
-        counter = counter + 1
-        H.append(h)
-        U1_ds.append(u1_ds)
-        U2_ds.append(u2_ds)
-        U1.append(u1_0)
-        U2.append(u2_0)
-        U1_U1ds.append(u1_0 - u1_ds)
-        U1_U1ds.append(u2_0 - u2_ds)
-        X.append(x_0)
-        Error_arr.append(S_nor / 15)
-        if error_control and S_nor < e / 32:
-            h = 2 * h
-            c2 += 1
-        C2.append(c2)
-        C1.append(c1)
-        if error_control and counter == N_max:
+    while x_0 <= x_end - h:
+        temp_1, temp_2 = RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h, v_max)
+        temp1_ds, temp2_ds = RK4_s(x_0, u1_0, u2_0, f_1, f_2, a, b, h / 2, v_max)
+        temp1_ds, temp2_ds = RK4_s(x_0 + h / 2, temp1_ds, temp2_ds, f_1, f_2, a, b, h / 2, v_max)
+        if temp_1 == v_max or temp_2 == v_max or temp1_ds == v_max or temp1_ds == v_max:
             break
+        S_nor = abs(((temp_1 - temp1_ds) ** 2 + (temp_2 - temp2_ds) ** 2) ** (1 / 2))
+        if error_control and S_nor > e:
+            h = h / 2
+            c1 += 1
+        else:
+            u1_0 = temp_1
+            u2_0 = temp_2
+            u1_ds = temp1_ds
+            u2_ds = temp2_ds
+            x_0 = x_0 + h
+            H.append(h)
+            U1_ds.append(u1_ds)
+            U2_ds.append(u2_ds)
+            U1.append(u1_0)
+            U2.append(u2_0)
+            U1_U1ds.append(u1_0 - u1_ds)
+            U1_U1ds.append(u2_0 - u2_ds)
+            X.append(x_0)
+            Error_arr.append(S_nor / 15)
+            if error_control:
+                if S_nor < e / 32:
+                    h = 2 * h
+                    c2 += 1
+            C2.append(c2)
+            C1.append(c1)
+        if error_control:
+            if counter >= N_max:
+                break
+        counter = counter + 1
 
-    data = [X, U1, U1_ds, Error_arr, H, C1, C2, U2, U2_ds,]
+    data = [X, U1, U1_ds, Error_arr, H, C1, C2, U2, U2_ds, ]
     return data
 
 
